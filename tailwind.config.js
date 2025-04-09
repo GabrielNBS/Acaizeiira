a3c5c3d83ecf333eb15b8a06b0cb9c1091dd9993
lohnogs <lorenaalbuquerque48@gmail.com>
2025-04-08 21:54:39 -0300

    colors at

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
-=======
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
@@ -25,4 +14,3 @@ module.exports = {
   variants:{},
   plugins: [],
 };
->>>>>>> 54ee30d0888bcad3684ab4e710b6e201ccdf6d1e
